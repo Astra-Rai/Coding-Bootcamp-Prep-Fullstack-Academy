d8e7e0523619a2d746d7c6dffeffc192f6a8f662
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-23 16:14:04 -0400

    01 Favorite Activity-Expressions, Vars & TDD
    Solved exercises using const variable and setting it a string value
    
    Link to code: https://codepen.io/Astra_Rai/pen/BeYEYY
    
    Information about const:  The const declaration creates a read-only reference to a value. It does not mean the value it holds is immutable, just that the variable identifier cannot be reassigned.
    
    Information about strings: The String global object is a constructor for strings or a sequence of characters.Link to information about const: 
    
    Link to information about strings:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
    
    Link to information about const:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const

---
+++ 01 Favorite Activity.js
@@ -0,0 +1,17 @@
+// YOUR CODE BELOW
+
+
+//set a variable, type of variable const, name of variable favoriteActivity
+//set variable, favorite activity to the the string, 'coding'
+
+const favoriteActivity = 'coding';
+
+
+//instructions
+describe('favoriteActivity', function() {
+
+  it('should be coding', function() {
+    expect(favoriteActivity).toEqual('coding');
+  });
+
+});
