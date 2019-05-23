231bf4d0765f3a4defde4d724738f2805d3e8753
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-23 14:55:30 -0400

    04 To Lowercase-Values and Types
    Exercise solved by applying string method toLowerCase ()
    Link to code: https://codepen.io/Astra_Rai/pen/pmaGOy
    Link to information on method toLowerCase():https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase

---
+++ 04 To Lowercase.js
@@ -0,0 +1,14 @@
+/*
+To Lowercase
+Create a String by surrounding characters with an apostrophe or quotation marks. Call the toLowerCase() method on the string.
+
+EX: "FULLSTACK ACADEMY".toLowerCase() OUTPUT: "fullstack academy";
+*/
+
+// YOUR CODE BELOW
+
+//set a variable to a string with all capital characters
+const makeSmaller = "FULLSTACK ACADEMY";
+
+//use toLowerCase() method to convert characters in strings to lower case
+console.log(makeSmaller.toLowerCase());//--> "FULLSTACK ACADEMY"
