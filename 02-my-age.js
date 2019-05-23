7ba11bf5685397e2c685b2b75ad84c9b32252601
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-23 14:39:12 -0400

    02-my-age-Values and Types
    Exercise solved using order of operations.
    Link to code: https://codepen.io/Astra_Rai/pen/WBMPNm
    Resource to learn order of operations: https://www.mathsisfun.com/operation-order-pemdas.html

---
+++ 02-my-age.js
@@ -0,0 +1,11 @@
+/*
+My Age
+Create an Arithmetic Expression that uses four mathematical operators (+,-,/,*) that returns your current age.
+
+EX: (9+1)*6/2-1;
+*/
+// YOUR CODE BELOW
+
+
+const myAge = (11.5 + 1) * 6/2-1;
+console.log(myAge);//--> 36.5
