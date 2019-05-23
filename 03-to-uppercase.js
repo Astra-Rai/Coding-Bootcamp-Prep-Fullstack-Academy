57239eaa67a90a4c46ac90ea03d6b90de0ba8f89
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-23 14:47:19 -0400

    03-to-uppercase-Values and Types
    Exercises solved using toUpperCase string method
    Link to code: https://codepen.io/Astra_Rai/pen/byLzoP 
    Link to information on toUpperCase() method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase

---
+++ 03-to-uppercase.js
@@ -0,0 +1,19 @@
+/*
+To Uppercase
+Create a String by surrounding characters with an apostrophe or quotation marks. Call the toUpperCase() method on the string.
+
+EX: "apples".toUpperCase() OUTPUT: "APPLES"
+*/
+
+// YOUR CODE BELOW
+
+
+//set a variable to a string with all lowercase characters
+
+const redFruit = "apples";
+
+//use string method, toUpperCase() to convert lowercase characters to uppercase
+
+//The toUpperCase() method returns the calling string value converted to uppercase (the value will be converted to a string if it isn't one).
+
+console.log(redFruit.toUpperCase());//--> "APPLES"
