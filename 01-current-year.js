e2d809b688569416e88455097e5cd7e1f83f7b0e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-23 14:37:16 -0400

    01-current-year-Values and Types
    Exercise solved using order of operations.
    Link to code: https://codepen.io/Astra_Rai/pen/OYQrdE
    Link to information on order of operations:https://www.mathsisfun.com/operation-order-pemdas.html

---
+++ 01-current-year.js
@@ -0,0 +1,15 @@
+/* 
+
+Current Year
+Create an Arithmetic Expression that uses three mathematical operators (+,-,/,*) that returns the date of the current year.
+
+EX: 2000 + 18 - 2 * 1;
+
+*/
+
+// YOUR CODE BELOW
+
+//Note: current year is 2019
+
+const currentYear = 2000 + (400/10) - 3*7;
+console.log(currentYear);
