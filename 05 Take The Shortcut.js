69e17fc248350c5354f5136470cf68f72d3072c1
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-23 21:00:46 -0400

    05 Take The Shortcut-Expressions, Var, and TDD
    Exercise similar to exercises 1 - 4
    Link to code:https://codepen.io/Astra_Rai/pen/xNWxda
    Link to more information on let:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let

---
+++ 05 Take The Shortcut.js
@@ -0,0 +1,14 @@
+let happyNum = 1000;
+
+// YOUR CODE BELOW
+//reset value of happyNum to five
+
+happyNum = 5;
+
+describe("happyNum", function() {
+
+  it("should be equal to 5", function() {
+    expect(happyNum).toEqual(5);
+  });
+
+});
